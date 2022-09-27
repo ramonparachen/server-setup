d4911e4acc28ada7c297b92bafd5b552823d363c
ramonparachen <ramonparachen@gmail.com>
2022-09-27 19:37:57 -0300

    "Manual script" version

--- guest-setup.sh
+++ guest-setup.sh
@@ -1,17 +1,5 @@
-#https://docs.docker.com/engine/install/debian/#install-using-the-repository 
-#1
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release
-#2
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-#3
-echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-#1
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
-#2
-
-#3
-sudo docker run hello-world
+#!/bin/sh
+sudo apt update && sudo apt-upgrade
+##INSTALL PHP
+#php7.4-xml provides php-dom php-simplexml php-xmlreader php-xmlwriter
+sudo apt install libxml2 php7.4 libapache2-mod-php7.4 php7.4-common php-ctype php-curl php7.4-xml php7.4-gd php7.4-json php7.4-mbstring php-posix php-zip php-mysql php7.4-bz2 php7.4-intl
