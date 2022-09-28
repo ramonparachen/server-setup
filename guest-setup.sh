4a32df06b6ac26d798a05d3ccb0a410468af292f
ramonparachen <ramonparachen@gmail.com>
2022-09-28 04:25:44 -0300

    Finished PHP section

--- guest-setup.sh
+++ guest-setup.sh
@@ -2,4 +2,4 @@
 sudo apt update && sudo apt-upgrade
 ##INSTALL PHP
 #php7.4-xml provides php-dom php-simplexml php-xmlreader php-xmlwriter
-sudo apt install libxml2 php7.4 libapache2-mod-php7.4 php7.4-common php-ctype php-curl php7.4-xml php7.4-gd php7.4-json php7.4-mbstring php-posix php-zip php-mysql php7.4-bz2 php7.4-intl
+sudo apt install libxml2 php7.4 libapache2-mod-php7.4 php7.4-common php-ctype php-curl php7.4-xml php7.4-gd php7.4-json php7.4-mbstring php-posix php-zip php-mysql php7.4-bz2 php7.4-intl php7.4-ldap smbclient php-imap php-bcmath php-gmp php-imagick ffmpeg libreoffice-core-nogui
