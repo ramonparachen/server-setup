b379b86dd6eee1f3bda3a917e295d5f3fd1be86b
ramonparachen <ramonparachen@gmail.com>
2022-09-29 01:32:40 -0300

    Basic DNS config for dnsmasq

---
+++ dns-setup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+sudo apt install dnsmasq
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.original
+
+echo "domain-needed" >> /etc/dnsmasq.conf
+echo "bogus-priv" >> /etc/dnsmasq.conf
+echo "no-resolv" >> /etc/dnsmasq.conf
+echo "domain=.casa" >> /etc/dnsmasq.conf
+echo "local=/.casa/" >> /etc/dnsmasq.conf
+echo "listen-address=127.0.0.1" >> /etc/dnsmasq.conf
+echo "" >> /etc/dnsmasq.conf
+echo "server=192.168.15.1" >> /etc/dnsmasq.conf
