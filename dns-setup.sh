8fe976f55df339662487576ac03757bfb92c4b41
ramonparachen <ramonparachen@gmail.com>
2022-09-29 15:32:01 -0300

    Fixed config errors

--- dns-setup.sh
+++ dns-setup.sh
@@ -6,8 +6,8 @@ echo "domain-needed" >> /etc/dnsmasq.conf
 echo "bogus-priv" >> /etc/dnsmasq.conf
 echo "no-resolv" >> /etc/dnsmasq.conf
 echo "expand-hosts" >> /etc/dnsmasq.conf
-echo "domain=.casa.net" >> /etc/dnsmasq.conf
-echo "local=/.casa.net/" >> /etc/dnsmasq.conf
-echo "listen-address=127.0.0.1" >> /etc/dnsmasq.conf
+echo "domain=casa.net" >> /etc/dnsmasq.conf
+echo "local=/casa.net/" >> /etc/dnsmasq.conf
+echo "listen-address=127.0.0.1,192.168.15.201" >> /etc/dnsmasq.conf
 echo "" >> /etc/dnsmasq.conf
 echo "server=192.168.15.1" >> /etc/dnsmasq.conf
