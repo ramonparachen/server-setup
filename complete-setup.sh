7613746aa04798afc8f5e65fc96c8cd8dc1baadf
ramonparachen <ramonparachen@gmail.com>
2022-10-06 11:27:39 -0300

    Starting a all in one script

---
+++ complete-setup.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+#Firewall {{{
+echo "flush ruleset" >> /etc/nftables.conf
+echo "table inet firewall {" >> /etc/nftables.conf
+echo "	chain inbound_ipv4 {" >> /etc/nftables.conf
+echo "		#icmp type echo-request limit rate 5/second accept" >> /etc/nftables.conf
+echo "	}" >> /etc/nftables.conf
+echo "	chain inbound_ipv6 {" >> /etc/nftables.conf
+echo "		icmpv6 type { nd-neighbor-solicit, nd-router-advert } accept" >> /etc/nftables.conf
+echo "		#icmp type echo-request limit rate 5/second accept" >> /etc/nftables.conf
+echo "	}" >> /etc/nftables.conf
+echo "	chain inbound {" >> /etc/nftables.conf
+echo "		type filter hook input priority 0; policy drop;" >> /etc/nftables.conf
+echo "		ct state vmap { established : accept, related : accept, invalid : drop }" >> /etc/nftables.conf
+echo "		iifname lo accept" >> /etc/nftables.conf
+echo "		meta protocol vmap { ip : jump inbound_ipv4, ip6 : jump inbound_ipv6 }" >> /etc/nftables.conf
+echo "		tcp dport { 53, 80, 8080, 443 } accept" >> /etc/nftables.conf
+echo "		udp dport { 53, 80, 8080, 443 } accept" >> /etc/nftables.conf
+echo "	}" >> /etc/nftables.conf
+echo "	chain forward {" >> /etc/nftables.conf
+echo "		type filter hook forward priority 0; policy drop;" >> /etc/nftables.conf
+echo "	}" >> /etc/nftables.conf
+echo "}" >> /etc/nftables.conf
+
+sudo systemctl enable nftables.service
+#}}}
+
+#DNS
+sudo apt install dnsmasq
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.original
+
+echo "domain-needed" >> /etc/dnsmasq.conf
+echo "bogus-priv" >> /etc/dnsmasq.conf
+echo "no-resolv" >> /etc/dnsmasq.conf
+echo "expand-hosts" >> /etc/dnsmasq.conf
+echo "domain=casa.net" >> /etc/dnsmasq.conf
+echo "local=/casa.net/" >> /etc/dnsmasq.conf
+echo "listen-address=127.0.0.1,192.168.15.201" >> /etc/dnsmasq.conf
+echo "" >> /etc/dnsmasq.conf
+echo "server=192.168.15.1" >> /etc/dnsmasq.conf
+
+#Docker
+
+#Nextcloud container
