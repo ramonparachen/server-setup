2d01330b642adc94f6954573ef1e3798d2801c5e
ramonparachen <ramonparachen@gmail.com>
2022-10-10 18:45:18 -0300

    Skipped a step on docker installation

--- complete-setup.sh
+++ complete-setup.sh
@@ -46,6 +46,7 @@ echo "server=192.168.15.1" >> /etc/dnsmasq.conf
 sudo apt-get -y ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 sudo docker run hello-world
