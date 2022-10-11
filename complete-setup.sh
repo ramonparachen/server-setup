c4e76db6a98638c5b17e1be8c92da34c6ca79a1a
ramonparachen <ramonparachen@gmail.com>
2022-10-11 18:50:42 -0300

    Typo

--- complete-setup.sh
+++ complete-setup.sh
@@ -43,7 +43,7 @@ echo "server=192.168.15.1" >> /etc/dnsmasq.conf
 
 #Docker {{{
 #https://docs.docker.com/engine/install/debian/
-sudo apt-get -y ca-certificates curl gnupg lsb-release
+sudo apt-get -y install ca-certificates curl gnupg lsb-release
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
