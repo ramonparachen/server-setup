169ea6ee7ea525c96420101f28afdc04bb3d7d7a
Ramon Parachen <ramonparachen@gmail.com>
2022-10-20 23:02:40 -0300

    Added uefi firmware package

--- host-setup.sh
+++ host-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #qemu-full or qemu-base for version without GUI
-sudo pacman -S qemu-full libvirt virt-manager dnsmasq
+sudo pacman -S qemu-full libvirt virt-manager dnsmasq edk2-ovmf
 
 sudo usermod -a -G libvirt ramon
 
